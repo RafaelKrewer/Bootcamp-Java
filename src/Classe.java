7e092410c89d787147af44aed26b3a1ea0bc7423
RafaelKrewer <rafaelkrewer5@gmail.com>
2025-03-19 17:35:31 -0300

    adicionando string

--- src/Classe.java
+++ src/Classe.java
@@ -1,6 +1,11 @@
 public class Classe {
-  
   public static void main(String[] args) {
-     System.out.print ("Olá, mundo!");
+    String brasil = "Brasil";
+    String nome = "Rafael";
+    int idade = 16;
+    int estados = 27;
+
+    System.out.println(nome + ", você tem " + idade + " anos");
+    System.out.println("Você mora no " + brasil + ", que tem " + estados + " estados");
   }
 }
