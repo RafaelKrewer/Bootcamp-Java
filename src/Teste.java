8fb9edb12067a54fa4b5988ce6be00c61c78386d
RafaelKrewer <rafaelkrewer5@gmail.com>
2025-03-25 16:42:20 -0300

    for

--- src/Teste.java
+++ src/Teste.java
@@ -1,8 +1,7 @@
 public class Teste {
   public static void main(String[] args) {
-    
 
-    for (int numero = 0; numero < 13; numero++) {
+    for (int numero = 0; numero < 100; numero++) {
       System.out.println(numero);
 
     }
