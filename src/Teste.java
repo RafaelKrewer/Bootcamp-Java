a472c62f5b26f0dad715a5eba5d38db67da33085
RafaelKrewer <rafaelkrewer5@gmail.com>
2025-03-25 15:34:31 -0300

    commit

--- src/Teste.java
+++ src/Teste.java
@@ -1,22 +1,11 @@
 public class Teste {
   public static void main(String[] args) {
+    int numero = 0;
+    numero++;
 
-  int numero10 = 10;
-  int numero32 = 32;
+    for (int i = 0; i < 13; i++) {
+      System.out.println(numero);
 
-  int resultado = numero10 * (numero32 + numero10);
-
-  System.out.println(resultado);
-
-    String primeiroNome = "Rafael";
-    String segundoNome = "Krewer";
-
-    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
-
-    System.out.println(nomeCompleto);
+    }
   }
-
-  public static String nomeCompleto(String primeiroNome, String segundoNome) {
-    return primeiroNome + (" ") + segundoNome;
-  } 
 }
