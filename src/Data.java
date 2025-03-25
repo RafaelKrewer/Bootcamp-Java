1e5077bf0561895f0e71beab8ddb955452d48df1
RafaelKrewer <rafaelkrewer5@gmail.com>
2025-03-25 19:15:32 -0300

    data de nascimento

---
+++ src/Data.java
@@ -0,0 +1,12 @@
+public class Data{
+  public static void main(String[] args) {
+    
+    int nascimento = 2008;
+
+    int ano = 2025 - nascimento;
+
+    System.out.println("Você tem " + ano + " anos");
+
+
+  }
+}
--- src/Teste.java
+++ src/Teste.java
@@ -1,14 +1,20 @@
 public class Teste {
   public static void main(String[] args) {
-    int a, b;
+    int valor1, valor2;
 
-    a = 5;
-    b = 5;
+    valor1 = 1;
+    valor2 = 2;
 
-    String resultado = "";
+    String valor = "";
 
-    resultado = a==b ? "verdadeiro" : "falso";
+    boolean simNão = valor1 == valor2;
+    if (simNão == true) {
+      valor = "Sim";
+    } else {
+      valor = "Não";
+    }
 
-    System.out.println(resultado);
+    System.out.println("Número Um é igual ao número Dois? " + valor);
+
   }
 }
